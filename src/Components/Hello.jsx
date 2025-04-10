6615d819ea8078510df604cb17842540e2969a7e
peace maker <peacemakerking80@gmail.com>
2025-04-10 02:42:11 +0100

    first commit

---
+++ src/Components/Hello.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "tailwindcss/tailwind.css"; // Import Tailwind CSS styles
+
+const Hello = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">The boss is back</p>
+    </div>
+  );
+};
+
+export default Hello;
